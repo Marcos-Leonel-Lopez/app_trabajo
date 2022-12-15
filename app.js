0a9aea220a1f524a58d5f052cb8ae54849755d28
Marcos Lopez <marcosleonellopez@gmail.com>
2022-12-15 11:06:43 -0300

    ultimos detalles

--- app.js
+++ app.js
@@ -1,39 +1,82 @@
 let personas = [];
-let n_id = 1;
+ let n_id;
+let n_ficha;
 const guardaLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
-const eliminaLocal = (clave) => { localStorage.removeItem(clave) }
+const eliminaLocal = (clave) => { localStorage.removeItem(clave) };
 
 class Personal {
-    constructor(Id, Nombre, Horas, Salario, Puesto, entrada, fecha_entrada, salida, fecha_salida, dia_s, dia_h, porHora) {
-        this.Id = Id,
-            this.Nombre = Nombre,
-            this.Horas = Horas,
-            this.Salario = Salario,
-            this.Puesto = Puesto,
-            this.entrada = entrada,
-            this.fecha_entrada = fecha_entrada,
-            this.salida = salida,
-            this.fecha_salida = fecha_salida,
-            this.dia_s = dia_s,
-            this.dia_h = dia_h,
-            this.porHora = porHora
+    constructor( id, Ficha, Nombre, Puesto, porHora) {
+        this.id = id,
+        this.Ficha = Ficha,
+        this.Nombre = Nombre,
+        this.Puesto = Puesto,
+        this.porHora = porHora
     }
+}
+
+function posicion(ident){
+    personas = JSON.parse(localStorage.getItem("trabajadoresLS"));
+    for( let j = 0; j < personas.length ; j++){
+
+        console.log(personas[j].Ficha);
+
+        if(personas[j].Ficha == ident){
+                k = j;
+             }
+        };
+        console.log(k);
+        return k
+}
+
+function maximo(array) {
+    return Math.max(...array.map(cUno => cUno.Ficha))
+}
+function identificarId(array,idDeseado) {
+    return array.filter( obj => idDeseado == obj.Ficha);
 }
 
 function muestra() {
     personas = JSON.parse(localStorage.getItem("trabajadoresLS"));
-    n_id = parseInt(localStorage.getItem("trabajador_listaLS"));
-    console.log(n_id);
     console.log(personas);
     personas.forEach(trabajador => {
-        listaDeTrabjadores.innerHTML += `<tr id="lista_${trabajador.Id}">
-        <td>${trabajador.Id}</td>
-        <td>${trabajador.Nombre}</td>
-        <td>${trabajador.Puesto}</td>
-        <td>${trabajador.porHora}</td>
-      </tr>`;
+        listaDeTrabjadores.innerHTML += `<tr>
+                                        <td>${trabajador.Ficha}</td>
+                                        <td>${trabajador.Nombre}</td>
+                                        <td>${trabajador.Puesto}</td>
+                                        <td>${trabajador.porHora}</td>
+                                    </tr>`;
     });
 }
+
+
+const cargarBD = async () => {
+    const resp = await fetch('http://localhost:3000/trabajadores')
+    const data = await resp.json()
+    console.log(data);
+    guardaLocal("trabajadoresLS", JSON.stringify(data));
+    guardaLocal("idLS", data.length+1);
+    guardaLocal("fichaLS", maximo(data)+1);
+};
+
+const postBD = async (nuevo) => {
+    const resp = await fetch('http://localhost:3000/trabajadores', {
+        method: 'POST',
+        body: JSON.stringify(nuevo),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    const data = await resp.json()
+    console.log(data);
+};
+
+const eliminaBD = async (id) => {
+    await fetch(`http://localhost:3000/trabajadores/${id}`, {
+        method: 'DELETE',
+    });
+}
+
+
 
 let registrar = document.getElementById("registro");
 let eliminar = document.getElementById("eliminar");
@@ -45,120 +88,155 @@ let i = 0;
 
 
 
-if (parseInt(localStorage.getItem("trabajador_listaLS")) && JSON.parse(localStorage.getItem("trabajadoresLS"))) {
+
+if (JSON.parse(localStorage.getItem("trabajadoresLS"))) {
     console.log(`LS cargado`);
     muestra();
 }
 else {
-    eliminaLocal("trabajadoresLS");
-    eliminaLocal("trabajador_listaLS");
-    console.log(`no hay nada en LS`);
+    cargarBD();
+    location.reload();
+}
+personas = JSON.parse(localStorage.getItem("trabajadoresLS"));
+n_ficha = parseInt(localStorage.getItem("fichaLS"));
+n_id = parseInt(localStorage.getItem("idLS"));
+// console.log(identificarId(personas,108));
+// console.log(personas.length);
+
+if (personas.length < 1) {
+    eliminarTodo.disabled = true;
 }
 
-registrar.addEventListener("submit", (e) => {
+
+registrar.addEventListener("submit", async (e) => {
     e.preventDefault();
-    let nomb = document.querySelector("#nombre").value.toUpperCase();
-    let puesto = document.querySelector("#puesto").value.toUpperCase();
-    let salario_hora = document.querySelector("#pesos").value;
-    // console.log(nomb);
-    // console.log(puesto);
-    // console.log(salario_hora);
     let registrar = e.target;
+    let nomb = registrar.nombre.value.toUpperCase();
+    let puesto = registrar.puesto.value.toUpperCase();
+    let salario_hora = parseFloat(registrar.pesos.value);
     if (nomb !== '' && puesto !== '' && salario_hora !== '') {
-        let i = parseFloat(salario_hora);
-        errorParrafo.innerText = null;
-        console.log(n_id);
-        listaDeTrabjadores.innerHTML += `<tr id="lista_${n_id}">
-        <td>${n_id}</td>
+        let i = parseFloat(salario_hora)
+        listaDeTrabjadores.innerHTML += `<tr">
+        <td>${n_ficha}</td>
         <td>${nomb}</td>
         <td>${puesto}</td>
         <td>${i}</td>
         `;
-        personas.push(new Personal(n_id, nomb, 0, 0, puesto, [], [], [], [], [], [], salario_hora));
+        let newObj = new Personal(n_id, n_ficha, nomb, puesto, salario_hora)
+        personas.push(newObj);
+        console.log(Personal);
+        console.log(newObj);
+        postBD(newObj);
         n_id++;
-        console.log(personas);
+        n_ficha++;
+        eliminarTodo.disabled = false;
         guardaLocal("trabajadoresLS", JSON.stringify(personas));
-        guardaLocal("trabajador_listaLS", n_id);
+        guardaLocal("idLS", n_id);
+        guardaLocal("fichaLS", n_ficha);
         Toastify({
             text: `${nomb} AGREGADO`,
             duration: 2000,
             className: "info",
             style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
             }
-          }).showToast();
-
+        }).showToast();
+        // location.reload();
     }
     else {
-        errorParrafo.innerText = "FALTAN DATOS!!!";
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'FALTAN DATOS'
+        })
     }
 });
 
 
-eliminar.addEventListener("submit", (e) => {
-    
-    let identidad = document.querySelector("#identidad").value;
-    console.log(identidad);
-    personas = JSON.parse(localStorage.getItem("trabajadoresLS"));
-    n_id = parseInt(localStorage.getItem("trabajador_listaLS"));
-    let i = parseFloat(identidad);
-    let x = i - 1;
+eliminar.addEventListener("submit", async (e) => {
+    e.preventDefault();
     let eliminar = e.target;
-    if (identidad !== '') {
+
+    let ident = parseInt(eliminar.identidad.value) || '';
+
+    personas = JSON.parse(localStorage.getItem("trabajadoresLS"));
+
+    console.log(ident);
+
+    
+
+    if (ident !== '') {
+
+        let i = identificarId(personas,ident)[0]?.id || "desconocido"; // identifica id dentro del db.json
+console.log(i);
         console.log(`antes detosty`);
-        
-        if (x < personas.length && x >= 0) {
-            
-            errorParrafo.innerText = null;
+        console.log(personas.length);
+        if (i != "desconocido") {
+            console.log(i);
+            let x =posicion(ident);
+            console.log(x);
+            console.log(i);
+            eliminaBD(i);
             personas.splice(x, 1);
-            console.log('lo elimino');
-            for (let j = x; j < personas.length; j++) {
-                personas[j].Id -= 1;
-            }
-            n_id -= 1;
             listaDeTrabjadores.remove();
             guardaLocal("trabajadoresLS", JSON.stringify(personas));
-            guardaLocal("trabajador_listaLS", n_id);
+            guardaLocal("idLS", n_id);
             muestra();
+            console.log(personas);
         }
         else {
             e.preventDefault();
-            errorParrafo.innerText = "ID DESCONOCIDO!!!";
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'ID DESCONOCIDO'
+            })
         }
     }
     else {
         e.preventDefault();
-        errorParrafo.innerText = "FALTAN DATOS!!!";
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'FALTAN DATOS'
+        })
     }
 });
 
-eliminarTodo.addEventListener('click',() =>{ 
+eliminarTodo.addEventListener('click', async () => {
+    eliminarTodo.disabled = true;
     Swal.fire({
         title: 'Esta seguro de eliminar todo?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonText: 'Si, seguro',
         cancelButtonText: 'No, no quiero'
-    }).then((result)=>{
-        if(result.isConfirmed){
+    }).then((result) => {
+        if (result.isConfirmed) {
             Swal.fire({
                 title: 'Borrados!',
                 icon: 'success',
                 text: 'El listado se limpio'
             })
-            listaDeTrabjadores.innerHTML='';
+            listaDeTrabjadores.innerHTML = '';
             eliminaLocal("trabajadoresLS");
-            eliminaLocal("trabajador_listaLS");
+            eliminaLocal("idLS");
+            eliminaLocal("fichaLS")
             n_id = 1;
             personas = [];
+            n_ficha = 101;
             console.log(`reset de peersonas e id`);
             guardaLocal("trabajadoresLS", JSON.stringify(personas));
-            guardaLocal("trabajador_listaLS", n_id);
+            guardaLocal("idLS", n_id);
+            guardaLocal("fichaLS",n_ficha);
         }
-    })   
- });
-    
-    
+        else {
+            eliminarTodo.disabled = false;
+        }
+    })
+});
+
+
 
 
 
